2bf5540eaf7022a9e45d55740433458bb5319abb
William Lee <leewp8@gmail.com>
2022-03-25 16:22:16 -0400

    work in progress

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,6 +1,11 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
-import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE } from './action-types'
+import { 
+  MOVE_CLOCKWISE, 
+  MOVE_COUNTERCLOCKWISE, 
+  SET_SELECTED_ANSWER, 
+  SET_QUIZ_INTO_STATE
+} from './action-types'
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
@@ -29,7 +34,14 @@ function quiz(state = initialQuizState, action) {
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch (action.type) {
+    case SET_SELECTED_ANSWER:
+      console.log(state)
+      return action.payload // this is wrong. needs fixed
+    default:
+      return state
+  }
+  
 }
 
 const initialMessageState = ''
