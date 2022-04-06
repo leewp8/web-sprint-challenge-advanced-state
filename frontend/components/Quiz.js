779abe8c2841efb4ceac1700b9daca0e7080bb88
William Lee <leewp8@gmail.com>
2022-04-05 23:37:38 -0400

    completed

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -9,20 +9,20 @@ function Quiz(props) {
   useEffect(() => {
     props.fetchQuiz()
   }, []);
-// console.log(selectAnswer)
 
 const handleSubmit = evt => {
   evt.preventDefault()
   props.postAnswer({quiz_id: quiz.quiz_id, answer_id: selectedAnswer.answer_id})
 }
 
-const isDisabled = () => {
-  if(document.getElementsByClassName('answer').value === 'answer selected') {
-    return true
-  } else {
-    return false
-  }
-}
+console.log('selected answer', selectedAnswer?.answer_id)
+// const disabled = () => {
+//   if(selectedAnswer?.answer_id === undefined) {
+//     return false
+//   } else {
+//     return true
+//   }
+// }
 
   return (
     <div id="wrapper">
@@ -35,20 +35,20 @@ const isDisabled = () => {
             <div id="quizAnswers">
               <div className={`answer${selectedAnswer?.answer_id===quiz.answers[0].answer_id? ' selected': ''}`}>
                 {quiz.answers[0].text}
-                <button onClick={() => selectAnswer(props.quiz.answers[0])}>
+                <button  onClick={() => selectAnswer(props.quiz.answers[0])}>
                   {`${selectedAnswer?.answer_id!==quiz.answers[0].answer_id? 'Select': 'SELECTED'}`}
                 </button>
               </div>
 
               <div className={`answer${selectedAnswer?.answer_id===quiz.answers[1].answer_id? ' selected': ''}`}>
               {quiz.answers[1].text}
-                <button onClick={() => selectAnswer(props.quiz.answers[1])}>
+                <button  onClick={() => selectAnswer(props.quiz.answers[1])}>
                 {`${selectedAnswer?.answer_id!==quiz.answers[1].answer_id? 'Select': 'SELECTED'}`}
                 </button>
               </div>
             </div>
 
-            <button id="submitAnswerBtn" onClick={handleSubmit} disabled={isDisabled()} >Submit answer</button> 
+            <button id="submitAnswerBtn" onClick={handleSubmit} disabled={selectedAnswer?.answer_id === quiz.answers[0].answer_id || selectedAnswer?.answer_id === quiz.answers[1].answer_id? false: true} >Submit answer</button> 
           </>
         ) : 'Loading next quiz...'
       }
@@ -67,4 +67,3 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps, {selectAnswer, fetchQuiz, postAnswer})(Quiz)
 
 
-// disabled={isDisabled()}
